c816061779e4ae29bf57ee3800c045f6577d0e4f
Nameless God <2582347430@qq.com>
2018-02-13 10:58:38 +0800

    Update miniko.py

--- examples/miniko.py
+++ examples/miniko.py
@@ -1,7 +1,7 @@
 """
 A simplest one page crawler -- Miniko desu!
 """
-from crawltools import get_source, save_img
+from crawltools import get_source, save_imgs
 src = get_source('https://konachan.net/post')
 links = src.xpath('//a[@class="directlink largeimg"]/@href')
 save_imgs(links)
