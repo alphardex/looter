fb5daa9e641671fe7ea04cda4c533b033f497263
Nameless God <2582347430@qq.com>
2018-02-13 10:59:13 +0800

    Update miniko_batch.py

--- examples/miniko_batch.py
+++ examples/miniko_batch.py
@@ -1,11 +1,11 @@
 """
 A simple linear spider, you can crawl whole site slowly with it.
 """ 
-from crawltools import get_source, save_img
+from crawltools import get_source, save_imgs
 def download(url):
     src = get_source(url)
     links = src.xpath('//a[@class="directlink largeimg"]/@href')
-    r = [save_img(link) for link in links]
+    save_imgs(links)
 
 if __name__ == '__main__':
     tasklist = reversed(list(f'https://konachan.net/post?page={9777-i}' for i in range(1, 9777)))
