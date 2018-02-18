c8f0b6ee7f6b26cb7f861329dcd7b686c5307c98
alphardex <2582347430@qq.com>
2018-02-18 10:02:00 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+import looter
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='looter',
+    version='v1.2',
+    description=(
+        'A python package aiming at avoiding unnecessary repetition in making common crawlers.'
+    ),
+    long_description=looter.__doc__,
+    author='alphardex',
+    author_email='2582347430@qq.com',
+    maintainer='alphardex',
+    maintainer_email='2582347430@qq.com',
+    license='MIT',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/alphardex/looter',
+    py_modules=['looter'],
+    scripts=['looter.py'],
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    "Intended Audience :: Developers",
+    "Intended Audience :: End Users/Desktop",
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    "Topic :: Internet",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Utilities"
+    ],
+    install_requires=[
+        'PyMySQL',
+        'selenium',
+        'requests',
+        'lxml',
+    ]
+)
