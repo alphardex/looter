bf5087e53e2a68254bc82074c56550d8e1b1135e
alphardex <2582347430@qq.com>
2018-02-26 13:49:09 +0800

    update version

--- looter.py
+++ looter.py
@@ -191,7 +191,7 @@ def link_mysql(fun):
 
 
 def cli():
-    argv = docopt(__doc__, version='v1.27')
+    argv = docopt(__doc__, version='v1.28')
     template = argv['<tmpl>']
     name = argv['<name>']
     if template not in ['data', 'image']:
