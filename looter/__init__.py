dcf12ba179d035efbfe47f17607c48b2ab93720b
alphardex <2582347430@qq.com>
2018-03-06 16:59:06 +0800

    update all

--- looter/__init__.py
+++ looter/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ["send_request", "get_source", "retrieve_html", "link_mysql", "run_selenium", "save_img", "save_imgs", "dynamic_get"]
+__all__ = ["send_request", "get_source", "retrieve_html", "link_mysql", "run_selenium", "save_img", "save_imgs", "dynamic_get", "get_alexa_rank"]
 
 from .looter import *
