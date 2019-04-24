fc693ce148d56ea655a54027c6ffd14c44b8e4ff
alphardex <2582347430@qq.com>
2019-04-24 11:27:08 +0800

    add spider - bangumi

---
+++ looter/examples/bangumi.py
@@ -0,0 +1,39 @@
+import os
+import json
+import re
+import looter as lt
+from pprint import pprint
+from concurrent import futures
+
+domain = 'http://bangumi.tv'
+encoding = 'utf-8'
+total = []
+
+format_date = lambda date: '-'.join([f'0{d}' if len(d) == 1 else d for d in re.sub(r'年|月|日', '-', date)[:-1].split('-')])
+
+
+def crawl(url):
+    tree = lt.fetch(url)
+    items = tree.css('ul#browserItemList li.item')
+    for item in items:
+        data = {}
+        data['title'] = item.css('h3 a.l::text').extract_first()
+        data['date'] = format_date(item.css('p.info::text').extract_first().strip().split(r'/')[1].strip())
+        data['url'] = f"{domain}{item.css('h3 a.l::attr(href)').extract_first()}"
+        pprint(data)
+        total.append(data)
+
+
+def generate():
+    with open('bangumi.json', encoding=encoding) as i, open('bangumi.md', 'w', encoding=encoding) as o:
+        data = json.loads(i.read())
+        o.writelines(f"{i+1}. [{d['title']}]({d['url']}) {d['date']}\n" for i, d in enumerate(data))
+
+
+if __name__ == '__main__':
+    tasklist = [f'{domain}/anime/list/399538/collect?orderby=date&page={n}' for n in range(1, 4)]
+    with futures.ThreadPoolExecutor(20) as executor:
+        executor.map(crawl, tasklist)
+    lt.save_as_json(total, name='bangumi.json', sort_by='date', order='desc')
+    generate()
+    os.remove('bangumi.json')
